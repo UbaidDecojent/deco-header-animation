6b11640fbee5036f7078b2e510ea770c691c9d2a
ubaidDecojent <ubaid.laghari@decojent.com>
2021-11-22 16:03:08 +0500

    smoothness fix

--- js/index.js
+++ js/index.js
@@ -9,15 +9,86 @@
 //   y: -(height - document.documentElement.clientHeight),
 //   scrollTrigger: {
 //     trigger: document.body,
-//     start: "top top",
-//     end: "bottom bottom",
+//     start: "40% 40%",
+//     end: "+=200%",
 //     scrub: 1,
 //     markers:true
 //   }
 // });
 
+// var html = document.documentElement;
+// var body = document.body;
+
+// var scroller = {
+//   target: document.querySelector("#scroll-container"),
+//   ease: 0.05, // <= scroll speed
+//   endY: 0,
+//   y: 0,
+  
+//   resizeRequest: 1,
+//   scrollRequest: 1,
+// };
+
+// var requestId = null;
+
+// TweenLite.set(scroller.target, {
+//   rotation: 0.01,
+//   force3D: true
+// });
+
+// window.addEventListener("load", onLoad);
+
+// function onLoad() {    
+//   updateScroller();  
+//   window.focus();
+//   window.addEventListener("resize", onResize);
+//   document.addEventListener("scroll", onScroll); 
+// }
+
+// function updateScroller() {
+  
+//   var resized = scroller.resizeRequest > 0;
+    
+//   if (resized) {    
+//     var height = scroller.target.clientHeight;
+//     body.style.height = height + "px";
+//     scroller.resizeRequest = 0;
+//   }
+      
+//   var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+
+//   scroller.endY = scrollY;
+//   scroller.y += (scrollY - scroller.y) * scroller.ease;
+
+//   if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+//     scroller.y = scrollY;
+//     scroller.scrollRequest = 0;
+//   }
+  
+//   TweenLite.set(scroller.target, { 
+//     y: -scroller.y 
+//   });
+  
+//   requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+// }
+
+// function onScroll() {
+//   scroller.scrollRequest++;
+//   if (!requestId) {
+//     requestId = requestAnimationFrame(updateScroller);
+//   }
+// }
+
+// function onResize() {
+//   scroller.resizeRequest++;
+//   if (!requestId) {
+//     requestId = requestAnimationFrame(updateScroller);
+//   }
+// }
+
+
 const tl = gsap.timeline({
-    defaults: {scaleX:1, scaleY:0.8, ease: 'slow'},
+    defaults: {scale:1, ease: 'slow'},
     scrollTrigger: {
       trigger: ".h-video",
       scrub: true,
@@ -25,15 +96,17 @@ const tl = gsap.timeline({
       start: "50% 50%",
       end: "+=100%",
       // markers:true
-      
     }
   })
 
+  
   .to(".video", {
-    scaleX: 1.67,
-    scaleY:1.7,
+    scale: 1.8,
+    // scale: 1.8,
     ease: "slow"
   })
+
+
 
   
 
